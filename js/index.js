be066f653f3856ec4901b65ff273d2c9d2e5897e
andriqwl <andriank4393@gmail.com>
2026-03-01 21:34:39 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,14 @@
+const inputEl = document.querySelector("#myInput");
+const buttonEl = document.querySelector("#myButton");
+buttonEl.value = inputEl.textContent;
+
+const imageLink1El = document.querySelector("[src='./img/photo1.jpg']");
+imageLink1El.src = "./img/img3.avif";
+
+const Link2El = document.querySelector("#link");
+Link2El.href = "https://uk.minecraft.wiki/w/Bedrock_Edition";
+const imageLink2El = document.querySelector("[src='./img/img3.avif']");
+imageLink2El.alt = "Minecraft";
+
+const listEl = document.querySelector("#newList");
+listEl.firstElementChild.textContent = "Fallout";
